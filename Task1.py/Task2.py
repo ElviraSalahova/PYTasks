23e40144cb23bd420b9d81a9fb09f6f625fab0e1
«Elvira» <elvira.salahova@gmail.com>
2023-07-02 22:26:55 +0400

    task1

---
+++ Task1.py/Task2.py
@@ -0,0 +1,23 @@
+# Задача 10: На столе лежат n монеток. Некоторые из них лежат вверх решкой, а некоторые – гербом. 
+# Определите минимальное число монеток, которые нужно перевернуть, чтобы все монетки были повернуты вверх одной и той же стороной. 
+# Выведите минимальное количество монет, которые нужно перевернуть
+
+# coins = input("Input coins: ")
+# up ='1'
+
+# upcount = 0
+# downcount = 0
+
+# for i in coins:
+#     if i == up:
+#         upcount +=1
+#     else:
+#         downcount +=1
+# if upcount > downcount:
+#     print(f'count of coins: {downcount}')
+# elif downcount > upcount:
+#     print(f'count of coins: {upcount}')
+# else:
+#     print(f'count of coins: {downcount}') 
+
+
